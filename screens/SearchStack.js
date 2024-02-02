6c67e87b8ca8ef6692f14eae4f3ac9177578d0c7
HirokiTakaya <99106476+HirokiTakaya@users.noreply.github.com>
2024-02-02 12:40:09 -0800

    Update SearchStack.js

--- screens/SearchStack.js
+++ screens/SearchStack.js
@@ -1,4 +1,4 @@
-// SearchStack.js
+
 import { createStackNavigator } from '@react-navigation/stack';
 import SearchScreen from './SearchScreen';
 import MovieDetailsScreen from './MovieDetail'; 
@@ -6,7 +6,7 @@ import TVScreen from './TVScreen';
 import TVDetails from './TVDetailsScreen';
 const SearchStack = createStackNavigator();
 
-// SearchStack.js
+
 export const SearchStackScreen = () => (
     <SearchStack.Navigator>
       <SearchStack.Screen name="Search" component={SearchScreen} />
